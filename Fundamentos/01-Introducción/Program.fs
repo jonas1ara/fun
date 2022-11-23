5584b05b496e261d9e522718ed99150de4460fec
Jonas Lara <adrianlr2515@outlook.com>
2022-11-22 18:42:00 -0600

    UPDAte

--- Fundamentos/01-Introducción/Program.fs
+++ Fundamentos/01-Introducción/Program.fs
@@ -1,11 +1,11 @@
 ﻿open System
 
 let HolaYouTube() =  
-	printf "Dime tú nombre: "
+    printf "Dime tú nombre: "
 
     let nombre = Console.ReadLine()
     
-    printfn "Hola %s, bienvenido al curso de FSharp en YouTube" nombre
+    printfn "Hola %s, bienvenido al curso FSharp en YouTube" nombre
     // Console.WriteLine( "Hola {0} desde FSharp", nombre);
 
 HolaYouTube()
