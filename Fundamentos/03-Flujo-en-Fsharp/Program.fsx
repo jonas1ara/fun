1431ba4ad8cba5576f8c6bf47274a98dbabffecb
Jonas Lara <adrianlr2515@outlook.com>
2022-12-07 19:27:07 -0600

    Fsharp ML

--- Fundamentos/03-Flujo-en-Fsharp/Program.fsx
+++ Fundamentos/03-Flujo-en-Fsharp/Program.fsx
@@ -1,2 +1,95 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿//Lógica condicional//
+
+//Ramas en el código//
+
+// let age = 17
+// if age >= 18
+// then printfn "Eres mayor de edad"
+// else printfn "Eres menor de edad"
+
+
+//Expresión ternaria//
+
+// let age = 17
+// //unit es un valor que actúa como marcador de posición cuando no se devuelve ningún valor real. Puede pensar en él como void o None.
+// let mensaje = if age >= 18 then "Eres mayor de edad" else "Eres menor de edad"
+// printfn "%s" mensaje
+
+//Elif//
+//la construcción elif toma un valor booleano que se debe agregar justo después de if. La construcción elif se ejecuta si if se evalúa como false. Este es un ejemplo de cómo usar elif
+
+// let valorCarta = 141
+// let descripcionCarta = if valorCarta = 1 then "Ace" elif valorCarta = 14 then "Ace14" else "A card"
+// printfn "%s" descripcionCarta
+
+//Naipes//
+// let valorCarta = 14
+    
+// let descripcionCarta =
+//     if valorCarta = 1 then "Ace"
+//     elif valorCarta = 11 then "Jack"
+//     elif valorCarta = 12 then "Queen"
+//     elif valorCarta = 13 then "King"
+//     else string valorCarta
+
+// printfn "%s" descripcionCarta
+
+//Bucles, si bucles//
+
+//for ... in
+
+// let list = [1; 2; 3; 4; 5] //Expresión enumerable
+// for i in list do //Patrón de enumeración y expresión enumerable
+//    printf "%d " i //Expresión de cuerpo
+
+// printfn ""
+
+// //for ... to
+
+// for i = 1 to 10 do
+//   printf "%i " i  // prints 1 2 3 4 5 6 7 8 9 10
+
+// printfn ""
+// //for ... downto
+
+// for i = 10 downto 1 do
+//   printf "%i " i  // prints 10 9 8 7 6 5 4 3 2 1
+
+// printfn ""
+
+//while ... do
+
+// let mutable quit = false
+// let no = 11
+// while not quit do //Mientras quit sea falso:not
+//     printf "Adivina un número: "
+//     let guess = System.Console.ReadLine() |> int
+//     if guess = no then
+//       quit <- true //Si adivinaste el número, quit es verdadero
+//       printfn "Adivinaste, %i es el número secreto" no
+//     else
+//       printfn "%i es incorrecto" guess
+
+//Naipes con bucles//
+
+let suit (no:int) : string = 
+    let suitNo:int = no / 13 //Dividimos el número de la carta entre 13 para obtener el número de palo
+    if suitNo = 0 then "Hearts"
+    elif suitNo = 1 then "Spades"
+    elif suitNo = 2 then "Diamonds"
+    else "Clubs"
+
+//La función se denomina cardDescription y el parámetro de entrada es card
+let cardDescription (card: int) : string =
+    let cardNo: int = card % 13 //Obtenemos el resto de la división de card entre 13, que es el número de la carta en el palo
+    printfn "cardNo: %i" cardNo
+    if cardNo = 1 then "Ace"
+    elif cardNo = 11 then "Jack"
+    elif cardNo = 12 then "Queen"
+    elif cardNo = 0 then "King"
+    else string cardNo 
+    
+
+let cards = [1; 10; 34; 13]
+for card in cards do
+    printfn "%s of %s" (cardDescription card) (suit card)
--- Fundamentos/04-Funciones-en-Fsharp/Program.fsx
+++ Fundamentos/04-Funciones-en-Fsharp/Program.fsx
@@ -1,2 +1,92 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// //Fundamentos//
+// //let <function name> <parameters> = <function body>
+// let a = 1
+// let b = 2
+// let c = 3
+
+// let add a b = a + b
+
+// //En F# lo que se devuelve es la información de la última línea de una función
+
+// let addAndMultiply a b c = 
+//     let suma = a + b
+//     let producto = suma * c
+//     producto
+// //Si no se especifica el tipo de retorno, se infiere el tipo de retorno de la última expresión de la función
+
+// let sum = add a b
+// printfn "%d" sum
+// printfn "%d" (addAndMultiply a b c)
+
+// //Funciones con tipos explícitos//
+// let convert (a:string) :int = //Parametro de tipo string y retorno de tipo int
+//     int a
+
+// let cardFace (card:int) :string = 
+//    let no = card % 13
+//    if no = 1 then "Ace"
+//    elif no = 0 then "King"
+//    elif no = 12 then "Queen"
+//    elif no = 11 then "Jack"
+//    else string no
+
+// printfn "%s" (cardFace 11)
+
+//Patrones funcionales//
+//Composición: una composición combina varias funciones en una sola.
+//Canalización: una canalización comienza con un valor y, luego, llama secuencialmente a muchas funciones usando la salida de una función como entrada de la siguiente.
+
+//Composición//
+// let add2 a = a + 2
+// let multiply3 a = a * 3 
+// let addAndMultiply a =
+//     let sum = add2 a
+//     let product = multiply3 sum
+//     product
+
+// printfn "%i" (addAndMultiply 2) // 12
+
+// //Composición//
+// let add2 a = a + 2
+// let multiply3 a = a * 3 
+// let addAndMultiply = add2 >> multiply3
+
+// printfn "%i" (addAndMultiply 2) // 12
+
+//Canalización//
+
+// let list = [4; 3; 1]
+// let sort (list: int list) = List.sort list
+// let print (list: int list)= List.iter(fun x-> printfn "item %i" x) list
+
+// list |> print  // item 4 item 3 item 1
+// printfn"-----------------"
+// list |> sort |> print // item 1 item 3 item 4
+
+let cards = [21; 3; 1; 7; 9; 23]
+let cardFace card = 
+    let no = card % 13
+    if no = 1 then "Ace"
+    elif no = 0 then "King"
+    elif no = 12 then "Queen"
+    elif no = 11 then "Jack"
+    else string no
+
+let suit card =
+    let no = card / 13
+    if no = 0 then "Hearts"
+    elif no = 1 then "Spades"
+    elif no = 2 then "Diamonds"
+    else "Clubs"
+
+let shuffle list =
+    let random = System.Random()
+    list |> List.sortBy (fun x -> random.Next())
+
+let printCard card = printfn "%s of %s" (cardFace card) (suit card)
+
+let printAll list = List.iter(fun x -> printCard(x)) list
+
+let take (no:int) (list) = List.take no list
+
+cards |> shuffle |> take 3 |> printAll
