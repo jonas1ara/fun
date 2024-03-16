cb64c4d67ad4ac8cbb1823fd038fca1b0f99f1b7
Jonas Lara <61485427+jonas1ara@users.noreply.github.com>
2024-03-16 03:41:12 -0600

    Update Functions.fsx

---
+++ Sources/Functions.fsx
@@ -0,0 +1,24 @@
+
+let myFunction (a: float) (b: float) =
+    a + b
+
+type MyTupleAlias = float * float
+
+let myOtherFunction (a: float, b: float) =
+    a + b
+myOtherFunction (1.0, 2.0)
+
+let myInput = 2.0, 3.0
+
+let anotherFunction (myInput: MyTupleAlias) =
+    1.0
+
+anotherFunction myInput
+
+
+
+
+myFunction 1.0 2.0
+
+let myAdd1 = myFunction 1.0
+myAdd1 10.0
